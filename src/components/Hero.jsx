24ca05b530c8124592cd02d0ddff135f3edc1203
Piya-Shah <priya.shah@locafy.com>
2026-03-27 15:56:17 +0800

    Hero section updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,19 +3,41 @@ import Button from "./Button";
 function Hero() {
   return (
     <div className="section">
-      <div className="container flex flex-col gap-5">
-        <h1 className="text-2xl md:text-4xl font-bold text-(--text-primary) md:max-w-2xl">
-          Frontend Developer <br />
-          <span className="text-(--text-accent) ">Frontend Developer with 4+ years of experience</span>
-        </h1>
-        <p className="text-lg md:text-xl leading-relaxed text-(--text-secondary) md:max-w-2xl">
-          Hi, I’m Priya. I’m a frontend developer based in Perth with 4+ years of experience building responsive
-          websites, reusable UI components, and user-friendly digital experiences. I focus on clean code, performance,
-          and practical design.
-        </p>
-        <div className="flex flex-wrap gap-5">
-          <Button onClick={() => window.open("/assets/Priya_Shah_Resume_2026.pdf", "_blank")}>View CV</Button>
-          <Button onClick={() => window.open("#projects")}>View Projects</Button>
+      <div className="container flex flex-col md:flex-row gap-5 items-center justify-center ">
+        {/* Section One */}
+        <div className="flex-1 flex flex-col gap-5">
+          <h1 className="text-2xl md:text-4xl font-bold text-(--text-primary) md:max-w-2xl">
+            Frontend Developer <br />
+            <span className="text-(--text-accent) ">Frontend Developer with 4+ years of experience</span>
+          </h1>
+          <p className="text-lg md:text-xl leading-relaxed text-(--text-secondary) md:max-w-2xl">
+            Hi, I’m Priya. I’m a frontend developer based in Perth with 4+ years of experience building responsive
+            websites, reusable UI components, and user-friendly digital experiences. I focus on clean code, performance,
+            and practical design.
+          </p>
+          <div className="flex flex-wrap gap-5">
+            <Button onClick={() => window.open("/assets/Priya_Shah_Resume_2026.pdf", "_blank")}>View CV</Button>
+            <Button onClick={() => (window.location.hash = "#projects")}>View Projects</Button>
+          </div>
+        </div>
+        {/* Section Two */}
+        <div className="flex-1 flex flex-col gap-5 items-center justify-center">
+          <img src="/assets/priya.png" alt="Priya Shah" className="w-75 h-75 rounded-full object-cover" />
+          {/* Badges */}
+          <div className="flex flex-wrap gap-4 mt-5 items-center justify-center">
+            <span className="bg-(--primary) text-(--text-surface) border border-(--border) px-4 py-2 rounded-full text-sm font-medium">
+              4+ Years Experience
+            </span>
+            <span className="bg-(--primary) text-(--text-surface) border border-(--border) px-4 py-2 rounded-full text-sm font-medium">
+              Landing Pages & SEO Focused Themes
+            </span>
+            <span className="bg-(--primary) text-(--text-surface) border border-(--border) px-4 py-2 rounded-full text-sm font-medium">
+              25+ Websites Built
+            </span>
+            <span className="bg-(--primary) text-(--text-surface) border border-(--border) px-4 py-2 rounded-full text-sm font-medium">
+              Reusable UI Components & Scalable Architecture
+            </span>
+          </div>
         </div>
       </div>
     </div>
